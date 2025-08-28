d01e1c10fe1fa5cc2bad0bca38dbdc0eb4df3717
David Chen <davidchen@dyntechsolution.biz>
2025-08-28 16:21:52 +0900

    added bulma

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Word } from './word/word';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Word],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
